409008befa210872d2a51d42cb6b266b9dbab3e6
glooms <marcus.lacerda@live.se>
2019-02-18 19:53:14 +0100

    Database is reinitialized on deploy.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from service import app
+from service import app, db
+from service.models import init_db, drop_db
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000)
+    try:
+        init_db()
+    except:
+        db.session.rollback()
+        drop_db()
+    init_db()
