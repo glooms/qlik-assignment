7e4bb87df2e4dd26f208cb481c755e3b9d9cef56
glooms <marcus.lacerda@live.se>
2019-02-18 19:26:36 +0100

    Deployment in progress, forgot to change response type to json in user.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from service import app
 
-app.run()
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
--- service/blueprints/user.py
+++ service/blueprints/user.py
@@ -1,15 +1,21 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from service.models import interface
 
 
 user_blueprint = Blueprint('user', __name__)
 
 
+@user_blueprint.route('/', methods=['GET'])
+def index():
+    return 'Hello World!'
+
+
 @user_blueprint.route('/user', methods=['GET'])
 def users_get():
-    users = map(str, interface.get_all_users())
-    message = ' '.join(users)
-    return (message, 200)
+    data = {
+        'users': [user.to_dict() for user in users]
+    }
+    return (jsonify(data), 200)
 
 
 @user_blueprint.route('/user', methods=['POST'])
@@ -20,13 +26,25 @@ def user_create():
         return ('Error: No user name specified.', 400)
     user = interface.create_user(user_name)
     if not user:
-        return ('Error: Duplicate user name.', 400)
-    return (str(user), 200)
+        data = {
+            'error': 'Duplicate user with name: %s' % user_name
+        }
+        return (jsonify(data), 400)
+    data = {
+        'user': user.to_dict()
+    }
+    return (jsonify(data), 200)
 
 
 @user_blueprint.route('/user/<user_name>', methods=['GET'])
 def user_get(user_name):
     user = interface.get_user(user_name)
     if not user:
-        return ('Error: No user by the name ' + user_name, 404)
-    return (str(user), 200)
+        data = {
+            'error': 'No user by the name %s' % user_name
+        }
+        return (jsonify(data), 404)
+    data = {
+        'user': user.to_dict()
+    }
+    return (jsonify(data), 200)
